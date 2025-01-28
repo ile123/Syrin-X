d41917c8dc4b33f1232ace0d8fa40871af2d6b14
ile123 <ilario.batistic@gmail.com>
2025-01-28 20:20:51 +0100

    added firebase and update the .gitignore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,11 +2,12 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.ile.syrin_x"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.ile.syrin_x"
@@ -56,4 +57,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
 }
